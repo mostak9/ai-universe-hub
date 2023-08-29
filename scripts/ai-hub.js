df3a9bb3823aee85b409b6650dc235d1168759c5
mostak9 <mostak.ahm263@gmail.com>
2023-08-29 12:59:04 +0600

    add modal details in the modal box

--- scripts/ai-hub.js
+++ scripts/ai-hub.js
@@ -4,7 +4,7 @@ const dataLoad = async (isShowMore) => {
     const cardData = data.data.tools;
     displayData(cardData, isShowMore);
 }
-
+// display cards
 function displayData(cardData, isShowMore) {
     const cardContainer = document.getElementById('card-container');
     const seeMoreContainer = document.getElementById('see_more_container');
@@ -34,13 +34,13 @@ function displayData(cardData, isShowMore) {
                 <p class="text-[#585858] mt-3"><i class="fa-solid fa-calendar-days"></i> <span class=""> ${card.published_in}</span></p>
             </div>
             <div>
-                <button class="btn bg-transparent border-none"><i class="fa-solid fa-circle-arrow-right text-3xl" style="color: #ff582e;"></i></button>
+                <button onclick="modal_show.showModal(); modalDetails('${card.id}')" class="btn bg-transparent border-none"><i class="fa-solid fa-circle-arrow-right text-3xl" style="color: #ff582e;"></i></button>
             </div>
         </div>
         `;
         
         cardContainer.appendChild(cardDiv);
-        feature(card.id, card.features)
+        feature(card.id, card.features);
     })
 }
 
@@ -48,8 +48,9 @@ dataLoad();
 
 //features function
 function feature(id,data) {
-
+    console.log(data);
     const featureContainer = document.getElementById(id);
+    console.log(featureContainer);
     data.forEach(element => {
         const li = document.createElement('li');
         li.innerText = element;
@@ -60,4 +61,48 @@ function feature(id,data) {
 // show more handler 
 function showMore() {
     dataLoad(true);
+}
+
+
+// modal details section
+const modalDetails = async(Id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${Id}`)
+    const data = await res.json();
+    const details = data.data
+    // console.log(details);
+    const aiDetails = document.getElementById('ai-details');
+    const aiShowCase = document.getElementById('ai-showcase');
+    aiDetails.innerHTML ='';
+    aiShowCase.innerHTML ='';
+    const detailDiv = document.createElement('div');
+    detailDiv.innerHTML = `
+    <h1 class="text-2xl font-semibold">${details.description}</h1>
+    <div class="flex gap-4 justify-between mt-4">
+    <div class="bg-white rounded-sm text-center text-green-400 p-3"><p>${details.pricing[0].plan}</p><p>${details.pricing[0].price}</p></div>
+    <div class="bg-white rounded-sm text-center text-yellow-400 p-3"><p>${details.pricing[1].plan}</p><p>${details.pricing[0].price}</p></div>
+    <div class="bg-white rounded-sm text-center text-red-400 p-3"><p>${details.pricing[2].plan}</p><p>${details.pricing[0].price}</p></div>
+    </div>
+
+    <div class="flex flex-col md:flex-row gap-4 justify-between">
+    <div>
+    <h3 class="font-semibold mt-4">Features</h3>
+    <ul class="list-disc text-xs p-4">
+    <li>${details.features['1'].feature_name}</li>
+    <li>${details.features['2'].feature_name}</li>
+    <li>${details.features['3'].feature_name}</li>
+    </ul>
+    </div>
+
+    <div>
+    <h3 class="font-semibold mt-4">Integrations</h3>
+    <ul class="list-disc text-xs p-4" id="${Id+Id}">
+
+    </ul>
+    </div>
+    </div>
+    `
+    aiDetails.appendChild(detailDiv);
+
+    feature(Id+Id, details.integrations);
+    // console.log(details.integrations[1]);
 }
